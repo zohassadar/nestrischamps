ac5237627714281512b92187d744f5e74956afc4
Timothee Groleau <timothee.groleau@gmail.com>
2023-04-15 21:33:47 +0800

    feat: APM tracks runtime metrics

--- tracer.js
+++ tracer.js
@@ -1,6 +1,7 @@
 import tracer from 'dd-trace';
 
 tracer.init({
+	runtimeMetrics: true,
 	logInjection: true, // https://docs.datadoghq.com/tracing/other_telemetry/connect_logs_and_traces/nodejs/
 });
 
