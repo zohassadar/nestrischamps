5e9ee4df5451f1a7f0234c55b2ee16e578b9fc94
Timothee Groleau <timothee.groleau@gmail.com>
2023-04-15 19:41:01 +0800

    chore: use default APM config

--- tracer.js
+++ tracer.js
@@ -1,9 +1,17 @@
 import tracer from 'dd-trace';
 
-tracer.init();
+tracer.init({
+	logInjection: true, // https://docs.datadoghq.com/tracing/other_telemetry/connect_logs_and_traces/nodejs/
+});
 
+/*
 tracer.use('express', {
 	middleware: false,
 });
+
+tracer.use('pg', {
+	service: 'pg'
+});
+/**/
 
 export default tracer;
