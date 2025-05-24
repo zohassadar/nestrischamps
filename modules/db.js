704c847b34eb996ab6b776b8a8cdcd9dbda5b3b3
Timothee Groleau <timothee.groleau@gmail.com>
2025-05-24 18:49:02 +0800

    refactor: handle DB pool better

--- modules/db.js
+++ modules/db.js
@@ -2,60 +2,23 @@ import pg from 'pg';
 
 let pool;
 
-if (process.env.IS_PUBLIC_SERVER) {
-	pool = new pg.Pool({
-		connectionString: process.env.DATABASE_URL,
-		ssl: {
-			rejectUnauthorized: false,
-		},
-	});
+const isPublicServer = /^(1|on|true)$/i.test(process.env.IS_PUBLIC_SERVER);
 
-	// the pool will emit an error on behalf of any idle clients
-	// it contains if a backend error or network partition happens
-	pool.on('error', err => {
-		console.error('DB: Unexpected error on idle client', err);
-	});
-} else {
-	/*
-	// Fake Pool for local access
-	// TODO: make a sqlite version
+console.log(`DB initialization`, {
+	IS_PUBLIC_SERVER: isPublicServer,
+});
 
-	async function query(query, args) {
-		console.log('Executing query');
-		console.log(query);
-		console.log(args);
+pool = new pg.Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: isPublicServer,
+	},
+});
 
-		return {
-			rows: [ {} ]
-		};
-	}
-
-	module.exports = {
-		async connect() {
-			return {
-				query,
-			};
-		},
-
-		query,
-	};
-	/**/
-
-	console.log('DEV DB', process.env.DATABASE_URL);
-	pool = new pg.Pool({
-		connectionString: process.env.DATABASE_URL,
-		/*
-		ssl: {
-			rejectUnauthorized: false,
-		},
-		/**/
-	});
-
-	// the pool will emit an error on behalf of any idle clients
-	// it contains if a backend error or network partition happens
-	pool.on('error', err => {
-		console.error('DB: Unexpected error on idle client', err);
-	});
-}
+// the pool will emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on('error', err => {
+	console.error('DB: Unexpected error on idle client', err);
+});
 
 export default pool;
