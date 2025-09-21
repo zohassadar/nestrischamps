5ffab6f9a028a83b749dd9c86dc89a65d60114ee
zohassadar <zohassadar@gmail.com>
2025-09-21 19:02:43 +0000

    Update addresses.js

--- public/emu/addresses.js
+++ public/emu/addresses.js
@@ -5,7 +5,8 @@
 // id to [address, num_bytes]
 const gym6_data_maps = {
 	gameMode: [0xc0, 1], // gameMode
-	playState: [0x48, 1], // gameModeState
+	playState: [0x48, 1], // playState
+	gameModeState: [0xA7, 1], // gameModeState
 	completedRowXClear: [0x52, 1], // rowY (rowY is a terrible name in Rom)
 	completedRows: [0x4a, 4], // completedRow
 	lines: [0x50, 2], // lines
